b0098658adfad92ed4b6d73cc714c743ee39f3f0
luccas-lopes <lopesluccassilva@gmail.com>
2023-06-27 20:27:03 -0300

    setting newLoginForm

--- src/components/NewLoginForm.tsx
+++ src/components/NewLoginForm.tsx
@@ -1,3 +1,5 @@
+import logo from "../../src/logo.svg";
+
 export default function NewLoginhtmlForm() {
   return (
     <>
@@ -7,17 +9,13 @@ export default function NewLoginhtmlForm() {
             href="#"
             className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
           >
-            <img
-              className="w-8 h-8 mr-2"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-              alt="logo"
-            />
-            Flowbite
+            <img className="w-8 h-8 mr-2" src={logo} alt="logo" />
+            EscalaFácil
           </a>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in to your account
+                Faça seu login
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
                 <div>
@@ -25,14 +23,14 @@ export default function NewLoginhtmlForm() {
                     htmlFor="email"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Your email
+                    E-mail
                   </label>
                   <input
                     type="email"
                     name="email"
                     id="email"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@company.com"
+                    placeholder="nome@empresa.com"
                     required
                   />
                 </div>
@@ -41,7 +39,7 @@ export default function NewLoginhtmlForm() {
                     htmlFor="password"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Password
+                    Senha
                   </label>
                   <input
                     type="password"
@@ -68,7 +66,7 @@ export default function NewLoginhtmlForm() {
                         htmlFor="remember"
                         className="text-gray-500 dark:text-gray-300"
                       >
-                        Remember me
+                        Lembre-se de mim
                       </label>
                     </div>
                   </div>
@@ -76,22 +74,22 @@ export default function NewLoginhtmlForm() {
                     href="#"
                     className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
                   >
-                    htmlForgot password?
+                    Esqueceu sua senha?
                   </a>
                 </div>
                 <button
                   type="submit"
                   className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
-                  Sign in
+                  Entrar
                 </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Don’t have an account yet?{" "}
+                  Ainda não tem uma conta?{" "}
                   <a
                     href="#"
                     className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                   >
-                    Sign up
+                    Cadastre-se
                   </a>
                 </p>
               </form>
