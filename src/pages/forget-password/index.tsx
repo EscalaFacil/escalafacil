b6b9904fc327d49d1062834f4f708d3fa8167805
luccas-lopes <lopesluccassilva@gmail.com>
2023-06-30 15:12:29 -0300

    changing logo

--- src/pages/forget-password/index.tsx
+++ src/pages/forget-password/index.tsx
@@ -43,8 +43,8 @@ export default function ForgetPassword() {
                     onChange={(e) => setUserEmail(e.target.value)}
                   />
                 </div>
-                <button className="w-full text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-                  Entrar
+                <button className="w-full text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                  Redefinir
                 </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Já sabe a senha?{" "}
