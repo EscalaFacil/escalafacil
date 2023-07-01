80f481b5d1adbb3b9850acb02e5a85227295f633
luccas-lopes <lopesluccassilva@gmail.com>
2023-07-01 16:21:08 -0300

    removing link

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { router } from "@router";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, Link } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
