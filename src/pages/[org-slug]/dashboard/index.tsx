bd4269be6ffadd24e431205a9428898e910e1507
luccas-lopes <lopesluccassilva@gmail.com>
2023-07-04 21:21:35 -0300

    Testando

--- src/pages/[org-slug]/dashboard/index.tsx
+++ src/pages/[org-slug]/dashboard/index.tsx
@@ -1,10 +1,12 @@
+import { Modal } from "flowbite";
 import BottomNav from "../../../components/BottomNav";
+import CRUDLayout from "../../../components/CRUDLayout";
 
 export default function Dashboard() {
   return (
     <>
+      <CRUDLayout />
       <BottomNav />
-      <p>Dashboard</p>
     </>
   );
 }
