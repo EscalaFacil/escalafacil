78e7b30098c32b1ec64ef74c095a0a34eadda116
luccas-lopes <lopesluccassilva@gmail.com>
2023-07-04 21:01:05 -0300

    removing signout import

--- src/pages/[org-slug]/dashboard/index.tsx
+++ src/pages/[org-slug]/dashboard/index.tsx
@@ -1,11 +1,9 @@
-import { signOut } from "@/firebase/login";
 import BottomNav from "../../../components/BottomNav";
 
 export default function Dashboard() {
   return (
     <>
       <BottomNav />
-
       <p>Dashboard</p>
     </>
   );
