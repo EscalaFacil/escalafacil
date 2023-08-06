a04fdbf944c440c62b4c0d0dc13f2720a0862c10
luccas-lopes <lopesluccassilva@gmail.com>
2023-08-06 19:37:34 -0300

    new adds

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Link, useParams } from "react-router-dom";
+import { signOut } from "@/firebase/login";
 import logo from "src/logo.svg";
 
 export default function Navbar() {
@@ -71,6 +72,10 @@ export default function Navbar() {
               </li>
               <li>
                 <a
+                  onClick={async () => {
+                    await signOut();
+                    window.location.reload();
+                  }}
                   href="#"
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
--- src/pages/[org-slug]/reports/index.tsx
+++ src/pages/[org-slug]/reports/index.tsx
@@ -1,10 +1,7 @@
 import Navbar from "@components/Navbar";
-import useModal from "../../../../src/hooks/UseModal";
 import ReportLayout from "@components/ReportsLayout";
 
 export default function Reports() {
-  const { isOpen, toggle } = useModal();
-
   return (
     <>
       <Navbar />
