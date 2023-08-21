79152c6a1c2f61e943cf4412deb93ca73ff0b035
Luccas Lopes <lopesluccassilva@gmail.com>
2023-08-21 20:50:40 -0300

    refactor(#4): Adicionando checkbox de tarefa fin.

--- src/pages/[org-slug]/dashboard/index.tsx
+++ src/pages/[org-slug]/dashboard/index.tsx
@@ -26,8 +26,7 @@ export default function Dashboard() {
             type="button"
             className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
             data-modal-toggle="defaultModal"
-          >
-          </button>
+          ></button>
         </div>
         {/* Modal body */}
         <form action="#">
@@ -124,6 +123,20 @@ export default function Dashboard() {
                 defaultValue={""}
               />
             </div>
+          </div>
+          <div className="flex items-center mb-4">
+            <input
+              id="default-checkbox"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              htmlFor="default-checkbox"
+              className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Esta tarefa já foi finalizada
+            </label>
           </div>
           <button
             type="submit"
