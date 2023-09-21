fa457fe3fe6f998b363b8e544428a707bd3d5265
Luccas Lopes <lopesluccassilva@gmail.com>
2023-09-21 15:01:10 -0300

    refactor(#4): Corrigindo página de relatórios.

--- src/pages/[org-slug]/reports/index.tsx
+++ src/pages/[org-slug]/reports/index.tsx
@@ -36,12 +36,12 @@ export default function Reports() {
                     Tipo de Relatório
                   </label>
                   <select
+                    required
                     id="category"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   >
-                    <option selected>Selecione</option>
-                    <option value="Luccas">Atividades</option>
-                    <option value="Adriano">Usuários</option>
+                    <option value="at">Atividades</option>
+                    <option value="us">Usuários</option>
                   </select>
                 </div>
                 <div className="grid gap-4 mb-4 sm:grid-cols-2 max-w-sm">
